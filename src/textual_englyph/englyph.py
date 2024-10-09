c5cd3dde57d03b96e74e860be05d90a954387b29
Frisco Rose <friscorose@gmail.com>
2024-10-08 21:28:38 -0400

    Clean up a bit and add click test for reactive update

--- src/textual_englyph/englyph.py
+++ src/textual_englyph/englyph.py
@@ -1,16 +1,20 @@
-from textual.app import App, ComposeResult
 from textual.widget import Widget
-from textual.widgets import Static
 from textual.reactive import reactive
 from textual.strip import Strip
 
-from rich.console import Console
 from rich.segment import Segment
 from rich.style import Style
 
 class EnGlyph( Widget ):
+    phrase = reactive("Hello from englyph!")
 
-    def render_line( self, row:int)-> Strip:
+    def on_click( self ) -> None:
+        self.update( "It's George" )
+
+    def update( self, text) -> None:
+        self.phrase = text
+
+    def render_line( self, row:int) -> Strip:
         if row > 0:
             return Strip.blank(1)
-        return Strip( [Segment("Hello from englyph!", Style.parse("default on default"))], 18 )
+        return Strip( [Segment(self.phrase, Style.parse("default on default"))], 18 )
